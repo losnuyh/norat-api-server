a6ece5e271f31190016953454834e279186fe36b
COCOLMAN <iam@hyunsol.com>
2024-01-03 21:19:39 +0900

    스크립트 코드에 type 제거

--- generate_env_file.py
+++ generate_env_file.py
@@ -1,18 +1,12 @@
 import argparse
-import sys
 from dataclasses import fields
-from os import environ
-from typing import TYPE_CHECKING
 
 from boto3 import client
 
 from application.config_scheme import ApplicationConfigScheme
 
-from mypy_boto3_ssm import SSMClient
-
-
 parser = argparse.ArgumentParser(
-    prog='.env file generator',
+    prog=".env file generator",
 )
 parser.add_argument("-e", "--env", required=True, choices=["dev", "prod"])
 parser.add_argument("-d", "--destination", default=".env")
@@ -20,7 +14,7 @@ args = parser.parse_args()
 
 env = args.env
 file_destination = args.destination
-ssm_client: SSMClient = client("ssm")
+ssm_client = client("ssm")
 names = [f"/second/{env}/{key.name}" for key in fields(ApplicationConfigScheme)]
 
 size = 10
@@ -29,7 +23,7 @@ values = {}
 
 while True:
     parameters = ssm_client.get_parameters(
-        Names=names[cursor: cursor + size],
+        Names=names[cursor : cursor + size],
         WithDecryption=True,
     )["Parameters"]
     for param in parameters:
