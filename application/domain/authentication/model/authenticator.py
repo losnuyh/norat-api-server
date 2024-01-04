184dc81ab769f01c02d02cf649861dd1613f7574
COCOLMAN <iam@hyunsol.com>
2024-01-04 14:59:32 +0900

    비밀번호 인증기 모델 추가

--- application/domain/authentication/model/authenticator.py
+++ application/domain/authentication/model/authenticator.py
@@ -1,47 +1,28 @@
 from dataclasses import dataclass
-from datetime import datetime, timedelta, timezone
-
-from application.domain.authentication.error import AuthenticationFail
-from application.jwt import jwt_token_manager
-from application.jwt.token_type import PhoneAuthenticationTokenPayload
-
-from .token import PhoneToken
+import bcrypt
 
 
 @dataclass
-class AuthenticationPhone:
-    code: str
-    phone: str
-    expired_at: datetime
+class PasswordAuthenticator:
+    user_account: str
+    hashed_password: bytes
 
-    def _check_token(self, *, code: str) -> bool:
-        return self.code == code and self.expired_at >= datetime.now(tz=timezone.utc)
-
-    def get_token(self, *, code: str) -> PhoneToken:
-        if not self._check_token(code=code):
-            raise AuthenticationFail("code is not correct")
-        now = datetime.now(tz=timezone.utc)
-        payload = PhoneAuthenticationTokenPayload(
-            phone=self.phone,
-            expired_at=now + timedelta(minutes=10),
+    def check_password(self, password: str) -> bool:
+        return bcrypt.checkpw(
+            password.encode(),
+            self.hashed_password,
         )
 
-        jwt_token = jwt_token_manager.create_phone_authentication_token(
-            payload=payload,
-        )
-        return PhoneToken(access_token=jwt_token)
 
-
-def new_authentication_phone(
-    *,
-    code: str,
-    phone: str,
-):
-    if phone == "01047374146":
-        code = "00000"
-
-    return AuthenticationPhone(
-        code=code,
-        phone=phone,
-        expired_at=datetime.now(tz=timezone.utc) + timedelta(minutes=10),
+def new_password_authenticator(
+    user_account: str,
+    user_password: str,
+) -> PasswordAuthenticator:
+    hashed_password = bcrypt.hashpw(
+        user_password.encode(),
+        bcrypt.gensalt(14),
+    )
+    return PasswordAuthenticator(
+        user_account=user_account,
+        hashed_password=hashed_password,
     )
