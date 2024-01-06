43348314249eabd73ae7e74ccca4b070877b6283
sol <solhyungusthf@gmail.com>
2024-01-06 21:52:41 +0900

    user_id 필드 추가

--- application/domain/authentication/adaptor/output/store/table.py
+++ application/domain/authentication/adaptor/output/store/table.py
@@ -36,9 +36,15 @@ class PasswordAuthenticatorTable(Base):
     __tablename__ = "password_authenticator"
 
     id: Mapped[int] = mapped_column(primary_key=True, kw_only=True)
+    user_id: Mapped[int] = mapped_column(
+        nullable=False,
+        kw_only=True,
+        index=True,
+        unique=True,
+    )
     user_account: Mapped[str] = mapped_column(
         String(32),
-        kw_only=True, index=True, unique=True,
+        kw_only=True, index=True,
     )
     hashed_password: Mapped[str] = mapped_column(
         String(128),
